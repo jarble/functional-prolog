d690251a950dd0a0540420481f4bc83dcaa3d65a
jarble <jarble@gmail.com>
2021-04-13 11:54:34 -0500

    Update functional_prolog.pl

--- functional_prolog.pl
+++ functional_prolog.pl
@@ -2,18 +2,18 @@
 :- set_prolog_flag('double_quotes','chars').
 
 main :- functional_syntax((
-			{writeln(hello),Z1=1,writeln(Z1)},
-			F1=factorial(3),
-			{writeln(F1)}
-        ),true).
+			A = factorial(1),
+			(A==1,writeln(yes);A==2,writeln('no')),
+			writeln(factorial(3))
+		)).
 
-functional_syntax(factorial(N) :-
-	((N=1 -> 1);
-        N*factorial(N-1))
+functional_syntax(writeln(A) :-(
+	A1=A,{writeln(A1)})
 ).
 
-print1(N,true) :-
-	writeln(N).
+functional_syntax(factorial(N) :- (
+	N=1 -> 1; N*factorial(N-1)
+)).
 
 unify_if_subsumes(A,B) :- subsumes_term(A,B),A=B.
 
@@ -32,17 +32,14 @@ functional_syntax(Input,C) :-
 functional_syntax(Input,C) :-
 	Input =.. [Name|Params],
     member1(Name,[=,==,\=,>,<,>=,=<,not]),
-    writeln(Input),
+    % writeln(Input),
     functional_syntax(Params,Params1),
     Output =.. [Name|Params1],
     (Output,C=true;(\+Output),C=false).
 
 functional_syntax(Input,Output) :-
 	unify_if_subsumes((A->B;C),Input),
-	functional_syntax(A,A1),
-	(A1==true,
-	functional_syntax(B,Output);
-	A1==false,functional_syntax(C,Output)).
+	functional_syntax((A1=A,(A1==true,Output=B;A1==false,Output=C))).
 
 functional_syntax(Input,[A1|B1]) :-
 	unify_if_subsumes([A|B],Input),
@@ -54,16 +51,14 @@ functional_syntax(Input,Result) :-
 	functional_syntax(B,B1),(B1==false,Result=false;B1==true,Result=true)
 	).
 functional_syntax(Input,Result) :-
-	unify_if_subsumes((A;B),Input),(
-    (functional_syntax(A,A1),call(A1);
-    functional_syntax(B,B1),call(B1)) -> (Result = true);
-    (functional_syntax(A,A1),A1=false,Result=false)).
+	unify_if_subsumes((A;B),Input),functional_syntax(A,A1),
+	(A1==true,Result=true;A1==false,functional_syntax(B,Result)).
 
 functional_syntax(Input,Output1) :-
 	functional_syntax((A :- B)),
 	subsumes_term(A,Input),
 	A=Input,
-	writeln((A:-B)),
+	% writeln((A:-B)),
 	functional_syntax(B,Output1).
 
 member1(El, [H|T]) :-
